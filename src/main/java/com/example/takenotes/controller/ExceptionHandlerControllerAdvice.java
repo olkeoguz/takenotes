cbd02c255dcd0f335a8b19c77f549735cee307f8
Oğuz ÖLKE <70342517+olkeoguz@users.noreply.github.com>
2021-01-03 12:54:12 +0300

    Update ExceptionHandlerControllerAdvice.java

--- src/main/java/com/example/takenotes/controller/ExceptionHandlerControllerAdvice.java
+++ src/main/java/com/example/takenotes/controller/ExceptionHandlerControllerAdvice.java
@@ -9,16 +9,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.NoSuchElementException;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-security-example
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 9/29/17
- * Time: 10:17 AM
- * To change this template use File | Settings | File Templates.
- */
 @ControllerAdvice
 public class ExceptionHandlerControllerAdvice {
 
