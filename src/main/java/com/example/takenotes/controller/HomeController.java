dfaa1d40653ca9878b6cd63d6b4699cf91a6eaa0
Oğuz ÖLKE <70342517+olkeoguz@users.noreply.github.com>
2021-01-03 12:53:58 +0300

    Update HomeController.java

--- src/main/java/com/example/takenotes/controller/HomeController.java
+++ src/main/java/com/example/takenotes/controller/HomeController.java
@@ -5,16 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- * Created by IntelliJ IDEA.
- * Project : spring-boot-security-example
- * User: hendisantika
- * Email: hendisantika@gmail.com
- * Telegram : @hendisantika34
- * Date: 9/29/17
- * Time: 10:18 AM
- * To change this template use File | Settings | File Templates.
- */
 @Controller
 public class HomeController {
 
